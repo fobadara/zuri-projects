1e68a3bcc696bb458f03bc667aa17b157807f44f
fobadara <fobadara@gmail.com>
2022-05-28 16:52:52 +0100

    Write code for readfile

---
+++ python/reading_text_files.py
@@ -0,0 +1,24 @@
+# Read text from a file, and count the occurence of words in that text
+# Example:
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    with open(filename, 'r') as f:
+        return f.read()
+      
+def count_words():
+    text = read_file_content("python/story.txt")
+    # [assignment] Add your code here
+    words = text.split()
+    word_count = {}
+    
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+print(count_words())
